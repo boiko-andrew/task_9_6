422d3c9958bf72f48c5881a31e55c2118c28339f
Andrew <boiko_andrew@mail.ru>
2024-02-14 18:43:48 +0300

    Initial commit

---
+++ src/main/java/ru/netology/task_9_6/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package ru.netology.task_9_6;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Andrew")
+                .password("{noop}password")
+                .authorities("closed_endpoint");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .and()
+                .authorizeRequests().antMatchers("/opened_endpoint")
+                .permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/closed_endpoint")
+                .hasAuthority("closed_endpoint")
+                .and()
+                .authorizeRequests().anyRequest().authenticated();
+    }
+}
---
+++ src/test/java/ru/netology/task_9_6/Task96ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.task_9_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task96ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
